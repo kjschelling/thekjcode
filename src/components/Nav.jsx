a6e3202b74096e4e7a5d36f20992363af5ddceb6
KJ <kjschelling@KJs-MacBook-Pro.local>
2022-07-05 21:10:11 -0400

    added new resume

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,11 +6,11 @@ import clsx from "clsx";
 function Nav(props) {
     return (
         <div className="nav full flex font-roboto bg-white shadow-lg">
-            <div className="m-5 mx-auto ">
+            <div className="m-5 mx-auto">
                 <a href="#about_section" className={styles.navItem}>  About</a>
                 <a href="#project_section" className={styles.navItem}> Experience</a>
                 <a href="#contact" className={styles.navItem}> Contact</a>
-                <button href="https://drive.google.com/file/d/1RI3jzZh2nSk4mUqOtUN3i-FI2NDsADP0/view?usp=sharing" className={styles.navItem}> Resume</button>
+                <button href="https://drive.google.com/file/d/19H8b4UBCTSieEDJ399JORLIXpfsMD_Gt/view?usp=sharing" className={styles.navItem}> Resume</button>
             </div>
         </div>
     )
